9b4c194be523b7a671f07a8fb619361f5b2b4777
Janbolot <janbolotnr@gmail.com>
2020-11-29 02:53:10 +0600

    diclesson

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new string[] { "A", "B", "J", "I", "K", "L", "B", "B", "I", "K" };
+
+            foreach (var kvPair in CountGradesLinear(grades))
+            {
+                Console.WriteLine($"Grade '{kvPair.Key}' : {kvPair.Value} student(s)");
+            }
+        }
+        static Dictionary<string, int> CountGradesLinear(IEnumerable<string> grades)
+        {
+            var dictionary = new Dictionary<string, int>(); // associative array, map, dictionary
+
+            foreach (var grade in grades)
+            {
+                dictionary[grade] = dictionary.ContainsKey(grade)
+                    ? dictionary[grade] + 1
+                    : 1;
+                /*  if (dictionary.ContainsKey(grade)) dictionary[grade] += 1;
+                  else dictionary[grade] = 1;*/ // можно и так писать код
+            }
+            return dictionary;
+
+        }
+    }
+}
